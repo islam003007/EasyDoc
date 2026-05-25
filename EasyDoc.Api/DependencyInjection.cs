144368b4c8c618530c58427937bfdc845a120564
islam rafat <islam003007@gmail.com>
2026-05-25 15:16:02 +0300

    fix: fixed unauthorized responses

--- EasyDoc.Api/DependencyInjection.cs
+++ EasyDoc.Api/DependencyInjection.cs
@@ -5,6 +5,7 @@ using EasyDoc.Application.Constants;
 using EasyDoc.Infrastructure.Data;
 using EasyDoc.Infrastructure.Data.Identity;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyDoc.Api
 {
@@ -24,13 +25,42 @@ namespace EasyDoc.Api
 
                 options.Lockout.AllowedForNewUsers = true;
             })
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
 
             services.ConfigureApplicationCookie(options =>
             {
                 options.Cookie.Name = "EasyDoc.Auth";
                 // default options are fine.
+
+                options.Events = new Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationEvents
+                {
+                    OnRedirectToLogin = context =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        context.Response.ContentType = "application/json";
+
+                        return context.Response.WriteAsJsonAsync(new ProblemDetails
+                        {
+                            Status = 401,
+                            Title = "Unauthorized",
+                            Detail = "You must be logged in to access this resource."
+                        });
+                    },
+
+                    OnRedirectToAccessDenied = context =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                        context.Response.ContentType = "application/json";
+
+                        return context.Response.WriteAsJsonAsync(new ProblemDetails
+                        {
+                            Status = 403,
+                            Title = "Forbidden",
+                            Detail = "You don't have permission to access this resource."
+                        });
+                    }
+                };
             });
 
             services.AddAuthorization(options =>
